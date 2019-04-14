bc355132bc8cc477ad491ed09cfb98b3f21cb4e2
interro <email>
2019-04-14 10:32:39 +0300

    added verification service

--- demo/src/components/Passenger/Receive.js
+++ demo/src/components/Passenger/Receive.js
@@ -6,245 +6,132 @@ import { Grid, Col } from "../shared/grid";
 import Card from "../shared/ContentCard";
 import { ThemedButton, ThemedExtLink } from "../shared/elements";
 import SidebarLeft from "../shared/SidebarLeft";
-import isValid from "../../utils/validateCityIdInfo";
 import isMobile from "../../utils/isMobile";
 import AttestationModal from "../uport/AttestationContainer";
 import SERVICES from "../../constants/servicesKlm";
+import {
+    SubmitPersonalDataToKLMService,
+    SelfiePhotoClaim,
+    FacePrintClaim,
+    DocumentPhotoClaim,
+} from "../../sagas/klmVerificationService";
 
 class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      attestationModal: false
-    };
-  }
-  componentDidMount() {
-    /*if(!this.props.isLoggedIn) {
-      this.props.redirectToPassengerHome();
-    } else if(!isValid(this.props.data).valid) {
-      this.props.redirectToPassengerIdForm();
+    constructor(props) {
+        super(props);
+        this.state = {
+            attestationModal: false
+        };
     }
-    */
-  }
-  hideAttestationModal = () => {
-    this.setState({
-      attestationModal: false
-    });
-    this.props.redirectToCityIdReceived();
-  }
-  showAttestationModal = () => {
-    this.setState({
-      attestationModal: true
-    });
-  }
-  render() {
-    const { attestationModal } = this.state;
-    const { verification, data, redirectToCityIdReceived, redirectToHome } = this.props;
-    /*
-    if(!this.props.isLoggedIn || !isValid(this.props.data).valid)
-      return null;
-    */
-
-    let firstNameClaim = {
-        type: "firstName",
-        value: "John",
-        issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-        issuedDate: "2019-04-12",
-        signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
+    componentDidMount() {
     }
-    let lastNameClaim = {
-        type: "lastName",
-        value: "Doe",
-        issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-        issuedDate: "2019-04-12",
-        signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
+    hideAttestationModal = () => {
+        this.setState({
+            attestationModal: false
+        });
+    }
+    showAttestationModal = () => {
+        this.setState({
+            attestationModal: true
+        });
     }
 
-    let birthDateClaim = {
-      type: "birthDate",
-      value: "2000-01-01",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
+    passengerDid = "did:uport:0x000000000000000000000000000000000000000A"
 
-    let personalIdClaim = {
-      type: "personalId",
-      value: "3001112323233434",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
+    render() {
+        const { attestationModal } = this.state;
+        const { verification, data, redirectToHome } = this.props;
 
-    let documentIdClaim = {
-      type: "602243343454",
-      value: "2000-01-01",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
+        let passengerGeneratedClaims = [
+            SelfiePhotoClaim,
+            FacePrintClaim,
+            DocumentPhotoClaim
+        ]
 
-    let selfiePhotoClaim = {
-      type: "selfiePhoto",
-      value: "BASE64:f52f2f29ff8ff",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
+        let klmVerifiedClaims = SubmitPersonalDataToKLMService(this.passengerDid, passengerGeneratedClaims)
 
-    let facePrintClaim = {
-      type: "facePrint",
-      value: "0FFFAFAFFFFFFFFFFFFFFFFF...",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    let documentPhotoClaim = {
-      type: "documentPhoto",
-      value: "BASE64:ff8f8fff4f5ff",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    // these are new generated claims
-    let documentCertifiedClaim = {
-      type: "documentCertified",
-      value: true,
-      passengerId: "0x11486054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    let boardingpassValidClaim = {
-      type: "boardingpassValid",
-      value: true,
-      passengerId: "0x11486054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    let taxFreeRegionClaim = {
-      type: "taxFreeRegion",
-      value: true,
-      passengerId: "0x11486054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    let membershipProofClaim = {
-      type: "membershipProof",
-      value: true,
-      passengerId: "0x11486054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuer: "0xKLM00054a6ad2c0b685cd89ce0ba018e210d501e",
-      issuedDate: "2019-04-12",
-      signature: "0x31486054a6ad2c0b685cd89ce0ba018e210d501e"
-    }
-
-    let claims = [
-        firstNameClaim,
-        lastNameClaim,
-        birthDateClaim,
-        personalIdClaim,
-        documentIdClaim,
-        selfiePhotoClaim,
-        facePrintClaim,
-        documentPhotoClaim,
-        documentCertifiedClaim,
-        boardingpassValidClaim,
-        taxFreeRegionClaim,
-        membershipProofClaim,
-    ]
-
-    const CTA = () => (<Card.CTA>
-      {isMobile()
-        ? <ThemedExtLink themeId={SERVICES.PASSENGER_ID.id}
-            className="long"
-            secondary
-            href={verification.url}
-            onClick={redirectToHome}
-          >Receive City ID</ThemedExtLink>
-        : <ThemedButton
-            themeId={SERVICES.PASSENGER_ID.id}
-            className="long" secondary onClick={redirectToHome}
-          >
+        const CTA = () => (<Card.CTA>
+        {isMobile()
+            ? <ThemedExtLink themeId={SERVICES.PASSENGER_ID.id}
+        className="long"
+        secondary
+        href={verification.url}
+        onClick={redirectToHome}
+            >Receive City ID</ThemedExtLink>
+    : <ThemedButton
+        themeId={SERVICES.PASSENGER_ID.id}
+        className="long" secondary onClick={redirectToHome}
+            >
             Back to Home page
-          </ThemedButton>}
-    </Card.CTA>);
-    return (<Wrapper>
-      <Grid>
-        <SidebarLeft service={SERVICES.PASSENGER_ID} active={2} />
+        </ThemedButton>}
+        </Card.CTA>);
+        return (<Wrapper>
+            <Grid>
+            <SidebarLeft service={SERVICES.PASSENGER_ID} active={2} />
         <Col span={6}>
-          <Card CTA={CTA}>
+            <Card CTA={CTA}>
             <h2>Good News!</h2>
-            <p>Your information has been succesfully verified by KLM services. Your Passenger ID is ready to be issued.</p>
-            <p>Your submitted information was splitted into verifable claims. All claims are stored in your device and can be shared with asking your permission.</p>
-            <p>KLM verified Claims:</p>
 
-            <JsonClaims>
-              {claims.map((claim, i) => {
+        <b>Your did:</b>
+        <JsonClaims>
+        {this.passengerDid}
+        </JsonClaims>
+
+        <p>Your information has been succesfully verified by KLM services. Your Passenger ID is ready to be issued.</p>
+        <p>Your submitted information was splitted into verifable claims. All claims are stored in your device and can be shared with asking your permission.</p>
+        <p>Submitted data from App to KLM service with personal data:</p>
+
+        <p>Claims generated from uploaded selfie picture and passport picture in mobile App:</p>
+        <JsonClaims>
+        {passengerGeneratedClaims.map((claim, i) => {
                 let jsonString = JSON.stringify(claim, null, 2)
                 return (<JsonClaim key={i}><pre>{jsonString}</pre></JsonClaim>)
-              })}
-            </JsonClaims>
+            })}
+        </JsonClaims>
 
+        <p>KLM generated and verified claims from passenger submitted personal data:</p>
+        <JsonClaims>
+        {klmVerifiedClaims.map((claim, i) => {
+                let jsonString = JSON.stringify(claim, null, 2)
+                return (<JsonClaim key={i}><pre>{jsonString}</pre></JsonClaim>)
+            })}
+        </JsonClaims>
 
-              <p>KLM generated verified Claims:</p>
-
-              <JsonClaims>
-                  {claims.map((claim, i) => {
-                      let jsonString = JSON.stringify(claim, null, 2)
-                      return (<JsonClaim key={i}><pre>{jsonString}</pre></JsonClaim>)
-                  })}
-              </JsonClaims>
-
-            <hr />
-
-            <h4>What’s next?</h4>
-            <p>
-              Let’s make sure you have an access to Airport
-              whenever and wherever you need them. {SERVICES.PASSENGER_ID.entity} is
-              going to send your new ID claims to your uPort app.
-            </p>
-          </Card>
+        </Card>
         </Col>
         <Col span={3}>
-        </Col>
-      </Grid>
-      <AttestationModal
+            </Col>
+            </Grid>
+            <AttestationModal
         heading="Check your device"
         description="Tap 'Accept' in your uPort app to receive your claims"
         infoHeading="You're Interacting with..."
         issuer={{
-          heading: SERVICES.PASSENGER_ID.name,
-          subHeading: SERVICES.PASSENGER_ID.entity,
-          name: SERVICES.PASSENGER_ID.entity,
-          logo: SERVICES.PASSENGER_ID.icon,
-          colors: theme.colors[SERVICES.PASSENGER_ID.id]
+            heading: SERVICES.PASSENGER_ID.name,
+                subHeading: SERVICES.PASSENGER_ID.entity,
+                name: SERVICES.PASSENGER_ID.entity,
+                logo: SERVICES.PASSENGER_ID.icon,
+                colors: theme.colors[SERVICES.PASSENGER_ID.id]
         }}
         infoDetails={[{
-          heading: "Issued Date",
-          name: (new Date()).toDateString()
-        }]}
+                heading: "Issued Date",
+                name: (new Date()).toDateString()
+            }]}
         claimDetails={SERVICES.PASSENGER_ID.generatedClaims.map(c => ({
-          name: c.name,
-          value: data[c.id]
-        }))}
+                name: c.name,
+                value: data[c.id]
+            }))}
         show={data && attestationModal}
         onClose={this.hideAttestationModal}
         claim={{
-          [SERVICES.PASSENGER_ID.claim]: data
+            [SERVICES.PASSENGER_ID.claim]: data
         }} />
-    </Wrapper>)
-  }
-}
+        </Wrapper>)
+    }
+    }
 
-const Wrapper = styled.div``;
-const JsonClaims = styled.ol`
+    const Wrapper = styled.div``;
+    const JsonClaims = styled.ol`
     background: #EEEDF4;
     border-radius: 5px;
     margin: 10px 0;
@@ -252,6 +139,6 @@ const JsonClaims = styled.ol`
     max-height: 200px;
     overflow: scroll;
 `;
-const JsonClaim = styled.div``;
+    const JsonClaim = styled.div``;
 
-export default Landing;
+    export default Landing;
---
+++ demo/src/sagas/klmVerificationService.js
@@ -0,0 +1,120 @@
+//import EthrDID from 'ethr-did'
+
+let signClaim = function(claim) {
+    //const keypair = EthrDID.createKeyPair();
+    //const ethrDid = new EthrDID({...keypair});
+    //console.log("DID: " + ethrDid.did)
+
+    //const claimJWT = ethrDid.signJWT(claim)
+    //console.log(claimJWT)
+
+    //let decodedClaim = jwtLib.decodeJWT(await claimJWT)
+    //console.log(decodedClaim)
+    //console.log("Issuer: " + decodedClaim.payload.iss)
+
+    //return claimJWT
+    return "0x000KLM000SIGNATURE000000000000"
+}
+
+let approveProperty = function(property, passengerDid) {
+    property["did"] = passengerDid
+    property["issuedDate"] = [Date.now()]
+
+    let signature =  signClaim(property)
+    //console.log(signature)
+    property["signature"] = [signature]
+}
+
+export var SubmitPersonalDataToKLMService = function(passengerDid, passengerGeneratedClaims) {
+    let klmVerifiedClaims = [];
+    for (let i in passengerGeneratedClaims) {
+        // make structure copy
+        let letsApprove = JSON.parse(JSON.stringify(passengerGeneratedClaims[i]))
+        klmVerifiedClaims.push(letsApprove)
+        approveProperty(letsApprove, passengerDid)
+    }
+
+    let klmGeneratedClaims = [
+        FirstNameClaim,
+        LastNameClaim,
+        BirthDateClaim,
+        IsOver18Claim,
+        PersonalIdClaim,
+        DocumentIdClaim,
+        DocumentCertifiedClaim,
+        IsBoardingpassValidClaim,
+        IsTaxFreeRegionClaim,
+        HasMembershipClaim
+    ]
+    for (let j in klmGeneratedClaims) {
+        let letsApprove = klmGeneratedClaims[j]
+        klmVerifiedClaims.push(letsApprove)
+        approveProperty(letsApprove, passengerDid)
+    }
+
+    return klmVerifiedClaims
+}
+
+export var FirstNameClaim = {
+    type: "firstName",
+    value: "John"
+}
+
+export var LastNameClaim = {
+    type: "lastName",
+    value: "Doe",
+}
+export var BirthDateClaim = {
+    type: "birthDate",
+    value: "2000-01-01",
+}
+
+export var IsOver18Claim = {
+    type: "isOver18",
+    value: true,
+}
+
+export var PersonalIdClaim = {
+    type: "personalId",
+    value: "3001112323233434",
+}
+
+export var DocumentIdClaim = {
+    type: "documentId",
+    value: "602243343454",
+}
+
+export var SelfiePhotoClaim = {
+    type: "selfiePhoto",
+    value: "BASE64:f52f2f29ff8ff",
+}
+
+export var FacePrintClaim = {
+    type: "facePrint",
+    value: "0FFFAFAFFFFFFFFFFFFFFFFF",
+}
+
+export var DocumentPhotoClaim = {
+    type: "documentPhoto",
+    value: "BASE64:ff8f8fff4f5ff",
+}
+
+export var DocumentCertifiedClaim = {
+    type: "documentCertified",
+    value: true,
+}
+
+export var IsBoardingpassValidClaim = {
+    type: "isBoardingpassValid",
+    value: true,
+}
+
+export var IsTaxFreeRegionClaim = {
+    type: "isTaxFreeRegion",
+    value: true,
+}
+
+export var HasMembershipClaim = {
+    type: "membershipProof",
+    value: true,
+}
